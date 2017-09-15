d722fa2dad62a7c2c440f61b2400dae0aee95044
Benjamin Herbold <ben.herbold@me.com>
2017-09-15 13:44:10 +0200

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
 
-css_dir = "assets/css" # by Compass.app
-sass_dir = "assets/sass" # by Compass.app
-images_dir = "assets/css/images" # by Compass.app
-output_style = :compressed # :expanded or :nested or :compact or :compressed
-relative_assets = false # by Compass.app
-line_comments = true # by Compass.app
-sass_options = {:debug_info=>false} # by Compass.app
-sourcemap = false # by Compass.app
+css_dir = "assets/css" 
+sass_dir = "assets/sass"
+images_dir = "assets/css/images"
+output_style = :compressed
+relative_assets = false
+line_comments = true
+sass_options = {:debug_info=>false}
+sourcemap = false
